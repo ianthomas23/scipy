7e9c5553c776f17fb076abf125bc6890a271385d
Ian Thomas <ianthomas23@gmail.com>
2025-12-08 16:02:58 +0000

    Extra char length arguments in linalg cython wrappers

--- scipy/linalg/_matfuncs_expm.h
+++ scipy/linalg/_matfuncs_expm.h
@@ -22,37 +22,37 @@
 // BLAS and LAPACK functions used
 void saxpy_(int* n, float* sa, float* sx, int* incx, float* sy, int* incy);
 void scopy_(int* n, float* dx, int* incx, float* dy, int* incy);
-void sgemm_(char* transa, char* transb, int* m, int* n, int* k, float* alpha, float* a, int* lda, float* b, int* ldb, float* beta, float* c, int* ldc);
-void sgemv_(char* trans, int* m, int* n, float* alpha, float* a, int* lda, float* x, int* incx, float* beta, float* y, int* incy);
+void sgemm_(char* transa, char* transb, int* m, int* n, int* k, float* alpha, float* a, int* lda, float* b, int* ldb, float* beta, float* c, int* ldc, int, int);
+void sgemv_(char* trans, int* m, int* n, float* alpha, float* a, int* lda, float* x, int* incx, float* beta, float* y, int* incy, int);
 void sgetrf_(int* m, int* n, float* a, int* lda, int* ipiv, int* info);
-void sgetrs_(char* trans, int* n, int* nrhs, float* a, int* lda, int* ipiv, float* b, int* ldb, int* info);
+void sgetrs_(char* trans, int* n, int* nrhs, float* a, int* lda, int* ipiv, float* b, int* ldb, int* info, int);
 void slacn2_(int* n, float* v, float* x, int* isgn, float* est, int* kase, int* isave);
 void sscal_(int* n, float* sa, float* sx, int* incx);
 
 void daxpy_(int* n, double* sa, double* sx, int* incx, double* sy, int* incy);
 void dcopy_(int* n, double* dx, int* incx, double* dy, int* incy);
-void dgemm_(char* transa, char* transb, int* m, int* n, int* k, double* alpha, double* a, int* lda, double* b, int* ldb, double* beta, double* c, int* ldc);
-void dgemv_(char* trans, int* m, int* n, double* alpha, double* a, int* lda, double* x, int* incx, double* beta, double* y, int* incy);
+void dgemm_(char* transa, char* transb, int* m, int* n, int* k, double* alpha, double* a, int* lda, double* b, int* ldb, double* beta, double* c, int* ldc, int, int);
+void dgemv_(char* trans, int* m, int* n, double* alpha, double* a, int* lda, double* x, int* incx, double* beta, double* y, int* incy, int);
 void dgetrf_(int* m, int* n, double* a, int* lda, int* ipiv, int* info);
-void dgetrs_(char* trans, int* n, int* nrhs, double* a, int* lda, int* ipiv, double* b, int* ldb, int* info);
+void dgetrs_(char* trans, int* n, int* nrhs, double* a, int* lda, int* ipiv, double* b, int* ldb, int* info, int);
 void dlacn2_(int* n, double* v, double* x, int* isgn, double* est, int* kase, int* isave);
 void dscal_(int* n, double* sa, double* sx, int* incx);
 
 void caxpy_(int* n, EXPM_C* sa, EXPM_C* sx, int* incx, EXPM_C* sy, int* incy);
 void ccopy_(int* n, EXPM_C* dx, int* incx, EXPM_C* dy, int* incy);
-void cgemm_(char* transa, char* transb, int* m, int* n, int* k, EXPM_C* alpha, EXPM_C* a, int* lda, EXPM_C* b, int* ldb, EXPM_C* beta, EXPM_C* c, int* ldc);
-void cgemv_(char* trans, int* m, int* n, EXPM_C* alpha, EXPM_C* a, int* lda, EXPM_C* x, int* incx, EXPM_C* beta, EXPM_C* y, int* incy);
+void cgemm_(char* transa, char* transb, int* m, int* n, int* k, EXPM_C* alpha, EXPM_C* a, int* lda, EXPM_C* b, int* ldb, EXPM_C* beta, EXPM_C* c, int* ldc, int, int);
+void cgemv_(char* trans, int* m, int* n, EXPM_C* alpha, EXPM_C* a, int* lda, EXPM_C* x, int* incx, EXPM_C* beta, EXPM_C* y, int* incy, int);
 void cgetrf_(int* m, int* n, EXPM_C* a, int* lda, int* ipiv, int* info);
-void cgetrs_(char* trans, int* n, int* nrhs, EXPM_C* a, int* lda, int* ipiv, EXPM_C* b, int* ldb, int* info);
+void cgetrs_(char* trans, int* n, int* nrhs, EXPM_C* a, int* lda, int* ipiv, EXPM_C* b, int* ldb, int* info, int);
 void clacn2_(int* n, EXPM_C* v, EXPM_C* x, float* est, int* kase, int* isave);
 void csscal_(int* n, float* sa, EXPM_C* sx, int* incx);
 
 void zaxpy_(int* n, EXPM_Z* sa, EXPM_Z* sx, int* incx, EXPM_Z* sy, int* incy);
 void zcopy_(int* n, EXPM_Z* dx, int* incx, EXPM_Z* dy, int* incy);
-void zgemm_(char* transa, char* transb, int* m, int* n, int* k, EXPM_Z* alpha, EXPM_Z* a, int* lda, EXPM_Z* b, int* ldb, EXPM_Z* beta, EXPM_Z* c, int* ldc);
-void zgemv_(char* trans, int* m, int* n, EXPM_Z* alpha, EXPM_Z* a, int* lda, EXPM_Z* x, int* incx, EXPM_Z* beta, EXPM_Z* y, int* incy);
+void zgemm_(char* transa, char* transb, int* m, int* n, int* k, EXPM_Z* alpha, EXPM_Z* a, int* lda, EXPM_Z* b, int* ldb, EXPM_Z* beta, EXPM_Z* c, int* ldc, int, int);
+void zgemv_(char* trans, int* m, int* n, EXPM_Z* alpha, EXPM_Z* a, int* lda, EXPM_Z* x, int* incx, EXPM_Z* beta, EXPM_Z* y, int* incy, int);
 void zgetrf_(int* m, int* n, EXPM_Z* a, int* lda, int* ipiv, int* info);
-void zgetrs_(char* trans, int* n, int* nrhs, EXPM_Z* a, int* lda, int* ipiv, EXPM_Z* b, int* ldb, int* info);
+void zgetrs_(char* trans, int* n, int* nrhs, EXPM_Z* a, int* lda, int* ipiv, EXPM_Z* b, int* ldb, int* info, int);
 void zlacn2_(int* n, EXPM_Z* v, EXPM_Z* x, double* est, int* kase, int* isave);
 void zdscal_(int* n, double* sa, EXPM_Z* sx, int* incx);
 
